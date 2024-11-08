dda3930edec849cff3e0be32b852c0765fde8895
NameFarhan <Farhanalibwn1215@gmail.com>
2024-11-08 18:32:52 +0500

    Setup Folder Structure

---
+++ src/Layout/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useTheme } from "@emotion/react";
+import { Box } from "@mui/material";
+import React from "react";
+
+const Navbar = () => {
+  const theme = useTheme();
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+import Navbar from "../Layout/Navbar"
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    </>
+)
+}   
+
+export default Home
